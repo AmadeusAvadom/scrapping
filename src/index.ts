8079ee74e390835d5d0cc6a25ff7da01575cd1a6
AmadeusAvadom <judaslinarez@gmail.com>
2024-10-04 07:28:58 -0500

    Verificar numeros repetidos

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
 import 'dotenv/config'
 import { read } from "xlsx";
 import path from 'path'
+import axios from 'axios'
 
 type WorkStringsType = {
     t: string,
@@ -26,8 +27,9 @@ puppeteer.use(
 );
 
 const Start = async () => {
-    let contVeces: number = 101;
-    let content = undefined;
+    let contVeces: number = 1;
+    let iterador: number = 1;
+    let content: string;
     let CSV = "";
     try {
         // Consultar el contenido del CSV
@@ -39,53 +41,91 @@ const Start = async () => {
     }
     while (contVeces < 100) {
         // @ts-ignore
-        console.log("   Procesando numero: " + WorkStrings[contVeces].t);
-        const browser = await puppeteer.launch({
-            headless: false,
-            //slowMo: 400
-            args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process']
-        });
+        let queryContent: string = fs.readFileSync('datos.csv', 'utf-8');
 
-        const page = await browser.newPage();
-        page.goto(WEB)
-        await new Promise(r => setTimeout(r, 5000))
-        await page.click("button[class='v-btn v-btn--elevated v-btn--slim v-theme--light v-btn--density-default v-btn--size-default v-btn--variant-elevated']");
-        await page.click("input[id='input-3']")
-        await page.waitForSelector('#input-3')
-        await clearInput(page, { selector: '#input-3' })
-        await page.type('#input-3', WorkStrings[contVeces].t.toString());
-        await page.evaluate(() => {
-            window.scrollBy(0, window.innerHeight);
-        });
-        const { solved, error } = await page.solveRecaptchas();
-        if (solved) {
-            console.log('✔️ The captcha has been solved');
-        }
-        if (error) console.log(error);
+        do {
+            iterador++;
+            //console.log(iterador);
+        } while (queryContent.search(WorkStrings[iterador].t.toString()) != -1);
+        
+            console.log("   Procesando numero: " + WorkStrings[iterador].t);
+            const browser = await puppeteer.launch({
+                headless: false,
+                //slowMo: 400
+                args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process']
+            });
 
+            const page = await browser.newPage();
+            page.goto(WEB)
+            await new Promise(r => setTimeout(r, 5000))
+            await page.click("button[class='v-btn v-btn--elevated v-btn--slim v-theme--light v-btn--density-default v-btn--size-default v-btn--variant-elevated']");
+            await page.click("input[id='input-3']")
+            await page.waitForSelector('#input-3')
+            await clearInput(page, { selector: '#input-3' })
+            await page.type('#input-3', WorkStrings[iterador].t.toString());
+            await page.evaluate(() => {
+                window.scrollBy(0, window.innerHeight);
+            });
+            const iframe = await page.$eval('iframe', (e) => {
+                return e.src
+            });
+            /*console.log("   Haciendo peticion a la API");
+            await getCaptcha(iframe)
+            console.log("\n");*/
 
-        await page.click("button[class='v-btn v-btn--elevated v-theme--light bg-warning v-btn--density-default v-btn--size-x-large v-btn--variant-elevated mt-4']")
-        await new Promise(r => setTimeout(r, 5000))
-        const element = await page.$eval('.v-card', (e) => {
-            return e.innerHTML
-        });
+            const { solved, error } = await page.solveRecaptchas();
+            if (solved) {
+                console.log('✔️ The captcha has been solved');
+            }
+            if (error) console.log(error);
+            await page.click("button[class='v-btn v-btn--elevated v-theme--light bg-warning v-btn--density-default v-btn--size-x-large v-btn--variant-elevated mt-4']")
+            await new Promise(r => setTimeout(r, 5000))
+            const element = await page.$eval('.v-card', (e) => {
+                return e.innerHTML
+            });
+    
+            let preFragmento = element.slice(662);
+            let operador = preFragmento.split("<")[0];
+            let fecha = preFragmento.split("<")[4].substring(2);
+            CSV += `${WorkStrings[contVeces].t.toString()}, ${operador} \n`
+    
+            content = fs.readFileSync('datos.csv', 'utf-8')
+            fs.writeFileSync('datos.csv', content + '\n' + WorkStrings[contVeces].t.toString() + '; ' + operador + '; ' + fecha);
 
-        let preFragmento = element.slice(662);
-        let operador = preFragmento.split("<")[0];
-        let fecha = preFragmento.split("<")[4].substring(2);
-        CSV += `${WorkStrings[contVeces].t.toString()}, ${operador} \n`
+            await browser.close()
+            contVeces++;
+    }
 
-        content = fs.readFileSync('datos.csv', 'utf-8')
-        fs.writeFileSync('datos.csv', content + '\n' + WorkStrings[contVeces].t.toString() + '; ' + operador + '; ' + fecha);
+}
 
-        console.log(CSV);
+const getCaptcha = async (src: string) => {
+    const slice = src.split("=");
+    const key = slice[2].split("&");
+    const API_KEY = "27bb8dfbf288282aa317d93d8720d810";
+    const { data } = await axios.get(`https://ocr.captchaai.com/in.php?key=${API_KEY}&method=userrecaptcha&googlekey=${key[0]}&pageurl=https://mysite.com/page/with/recaptcha`);
+    let id_captcha: string;
+    id_captcha = data.split('|')
 
+    console.log("   ID del captcha: " + id_captcha[1]);
+    await new Promise(r => setTimeout(r, 15000))
+    let code = await axios.get(`https://ocr.captchaai.com/res.php?key=${API_KEY}&action=get&id=${id_captcha[1]}`)
+    console.log("   Respuesta " + code.data);
 
-        await browser.close()
-        contVeces++;
+    while (await queryApi(API_KEY, id_captcha[1]) == "CAPCHA_NOT_READY") {
+        console.log("   Reintentando");
+
+        await new Promise(r => setTimeout(r, 15000))
+        code = await queryApi(API_KEY, id_captcha[1])
+        console.log(code);
 
     }
 
+}
+
+const queryApi: any = async (API_KEY: string, captcha: any) => {
+
+    let rs = await axios.get(`https://ocr.captchaai.com/res.php?key=${API_KEY}&action=get&id=${captcha}`)
+    return rs.data
 }
 
 const clearInput = async (page, { selector }) => {
